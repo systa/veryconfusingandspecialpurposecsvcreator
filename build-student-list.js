d2bfacdb5085ab9b3e59fb3a868094a23d3630ac
Kari Systä (TAU) <systa@wks-91544-mac.ad.tuni.fi>
2020-09-09 13:35:00 +0300

    init

---
+++ build-student-list.js
@@ -0,0 +1,22 @@
+// NOT BE USED OR UNDERSTOOD SEPARATELY
+var fs = require("fs");
+var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
+var data = JSON.parse(stdinBuffer.toString());
+var res = data.results;
+console.log("Opnro;Nimi;Email");
+for (i in res) {
+    var line = "";
+    if (res[i].student_id != null) {
+	line += tuniid(res[i].student_id) + ";";
+	line += res[i].full_name + ";";
+	line += res[i].email;
+	console.log(line);
+    }
+}
+
+function tuniid(s) {
+    var i = s.indexOf("tuni.fi:");
+    if (i < 0)
+	return ("??");
+    return s.substr(i+8);
+}
